846ca9e7d4e9470313b5617bde618b877e17883b
wrakc <carlosveiga1618@gmail.com>
2020-12-09 14:58:02 -0300

    add basic structure of index.js functions that will render the website

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./assets/styles.css";
+
+(function() {
+  var weatherApp = {
+    init: function() {
+      this.cacheDom();
+      this.bindEvents();
+      this.render();
+    },
+    cacheDom: function() {
+      //search for elements in the DOM for DOM manipulation;
+    },
+    bindEvents: function() {
+      // for events on click or something passing the functions. 
+    },
+    render: function() {
+      //render page
+    },
+
+
+
+  }
+
+  weatherApp.init();
+})();
