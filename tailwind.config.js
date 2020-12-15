3135ded6bae455b02aa212488e8cc5bfd48fe68b
wrakc <carlosveiga1618@gmail.com>
2020-12-14 22:36:47 -0300

    enable purge configuration for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.html',
     './src/**/*.js',
-  ],
+    './src/**/*.css',]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
