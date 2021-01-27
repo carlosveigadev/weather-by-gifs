55ae0d9f923db9c9d2d61b30dcf63eddb6f4f5a9
wrakc <carlosveiga1618@gmail.com>
2021-01-27 14:00:26 -0300

    fix bug when more than 3 words was passed to fetch data from Giphy the api wouldnt work.

--- src/modules/getGifs.js
+++ src/modules/getGifs.js
@@ -3,6 +3,7 @@ import openModalError from './openModalError';
 async function getGifs(dataStatus) {
   let response;
   let gif;
+  dataStatus = dataStatus.split(' ').slice(0, 2).join(' ');
   try {
     response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=QswEirySAX3cPIt29dYs7BzkO919Pivk&q=${dataStatus}`,
       {
